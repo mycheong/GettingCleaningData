0a3bcbd7dd5f0bc8f19d2dea069bc09b0934f58a
mycheong <meiyen.cheong@gmail.com>
2015-11-22 15:19:20 +0200

    CourseProject_tidyDataset

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# GettingCleaningData
+README! 
+
+Course project requirement:
+Write an R script called run_analysis.R that does the following:-
+- Merge the training and the test sets to create one data set.
+- Extract only the measurements on the mean and standard deviation for each measurement. 
+- Assign descriptive activity names to name the activities in the data set
+- Appropriately label the data set with descriptive variable names. 
+- Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+-----------------------------------------------------------------  
+
+(A) To run the function run_analysis.R() 
+
+1. Copy the script file (run_analysys.R) to your working directory. 
+2. The following files should be in the working directory:-
+- features.txt
+- activity_labels.txt
+- subject_test.txt
+- subject_train.txt
+- X_test.txt
+- y_test.txt
+- X_train.txt
+- y_train.txt
+[Note : Some of these files might be originally stored in sub-directories. Move them to the directory given to the working directory.]
+3. Then source run_analysis.R
+4. Run the function by typing this in your R console, for example : X_tidy <- run_analysis() [The function does not need input argument]
+5. The function returns the tidy dataset X_tidy 
+
+
+(B) run_analysis() performs the following tasks:-
+1. Read in all the required data files (listed above) and make sure they are in appropriate dimension. 
+2. Set appropriate column names for the data sets and unique row names for X_test and X_train.
+3. Bind the "subject_id" and "activity_label" to the main measurement datasets X_test and X_train (added as new columns).  
+4. Merge these "new" training and test data frames (X_train, X_test) by binding their rows together to form X_merged.
+5. Remove duplicated columns in X_merged (duplicates are identified by their column names only, not the data) to get X_noDup.
+6. From X_noDup, sift out only mean and std measurements to form X_ms.
+7. Add a column to X_ms for "activity name" that corresponds to the record's activity label.   
+8. Re-label the columns of X_ms with appropriate names. (Note : Mostly modify the original names given in features.txt, e.g. removing repititions)
+9. Reaarrage the dataframe X_ms by melting it into id variables (subject ids and activity names) and measures variables (all columns containing measurement)
+10. Re-cast the melted data frame to form the tidy dataset X_tidy, where the subject_id and activity forms the row and the average of the measurements are in the columns  
+11. Rename the columns of X_tidy by adding a prefix "Avg" to indicate the values in the columns are the average over the given subject and activity
+12. The function returns the tidy dataset X_tidy
+
+ 
+
+
---
+++ run_analysis.R
@@ -0,0 +1,131 @@
+## run_analysis.R 
+## 21/11/2015 by mycheong
+##
+
+run_analysis <- function()
+{
+
+## Reading relevant data files into R
+##
+#filepath <- paste(directory, fname, sep="/")
+filepath <- "./features.txt"
+features <- read.csv(filepath, header=F, sep="")
+features <- features$V2
+
+filepath <- "./activity_labels.txt"
+act_label <- read.table(filepath)
+
+filepath <- "./subject_test.txt"
+subject_test <- read.table(filepath)
+
+filepath <- "./X_test.txt"
+X_test <- read.table(filepath)
+
+filepath <- "./y_test.txt"
+y_test <- read.table(filepath)
+
+filepath <- "./subject_train.txt"
+subject_train <- read.table(filepath)
+
+filepath <- "./X_train.txt"
+X_train <- read.table(filepath)
+
+filepath <- "./y_train.txt"
+y_train <- read.table(filepath)
+
+##
+## Naming the columns of the measurement datasets: 
+## Columns of X_test and X_trainas the elements in features 
+## Column of subject_train and subject_test as "subject_id"
+## Column of y_train and y_test as "activ_label"
+##
+colnames(X_test) <- features
+colnames(X_train) <- features
+
+colnames(subject_train)<- "subject_id"
+colnames(subject_test)<- "subject_id"
+colnames(y_train)<- "activ_label"
+colnames(y_test)<- "activ_label"
+
+##
+## Making the row names of X_train and X_test unique (for rbind later)
+##
+rownames(X_train) <- make.names(c(1:nrow(X_train)), unique = T)
+rownames(X_test) <- make.names(c(1:nrow(X_test))+7352, unique = T)
+
+##
+## Binding subject id and activity label to the columns of X_train and X_test
+##
+X_train<-cbind(X_train, subject_id = subject_train, activ_label = y_train)
+X_test<-cbind(X_test, subject_id = subject_test, activ_label = y_test)
+
+##
+## Binding the rows of X_train and X_test  together (they have same number of columns) 
+##
+X_merged <- rbind(as.matrix(X_train), as.matrix(X_test))
+X_merged <- as.data.frame(X_merged)
+
+##
+## Removing columns with duplicated column names
+##
+X_noDup <- X_merged[,-which(duplicated(features)==T)]
+
+##
+## Sifting out mean and std measurements
+##
+#X_ms <- X_merged[,c(grep("mean()", names(X_merged), fixed=T), grep("std()", names(X_merged), fixed=T), 562, 563)]
+X_ms <- X_noDup[,c(grep("mean()", names(X_noDup), fixed=T), grep("std()", names(X_noDup), fixed=T), 478, 479)]
+
+
+##
+## Giving descriptive name for activities in an added new column
+## Create a 1-variable data frame translating the activ_label to activity names
+## given in act_label
+## 
+activity_name <- character(nrow(X_ms))
+activity_name[which(X_ms$activ_label==1)]<- as.character(act_label[1,2])
+activity_name[which(X_ms$activ_label==2)]<- as.character(act_label[2,2])
+activity_name[which(X_ms$activ_label==3)]<- as.character(act_label[3,2])
+activity_name[which(X_ms$activ_label==4)]<- as.character(act_label[4,2])
+activity_name[which(X_ms$activ_label==5)]<- as.character(act_label[5,2])
+activity_name[which(X_ms$activ_label==6)]<- as.character(act_label[6,2])
+activity_name <- as.data.frame(activity_name)
+colnames(activity_name) <- "activ_name"
+
+##
+## Bind activity_name column to X_ms
+##
+X_ms <- cbind(X_ms, activity_name)
+
+
+## Appropriately label variables (columns) with descriptive names  
+## 
+## Here, only the typo in the original names given in features.txt are corrected. 
+## Specifically, replacing names with "BodyBody.." to "Body.."
+## Replacing "-" in the names with "_"
+##
+names(X_ms) <- sub("BodyBody", "Body", names(X_ms), fixed=T)
+names(X_ms) <- gsub("-", "_", names(X_ms))
+
+
+##
+## Create independent tidy data set X_tidy 
+## with the average of each variable for each activity and each subject.
+##
+X_melt <- melt(X_ms, id=c("subject_id", "activ_name"), measure.vars=c(names(X_ms[1:66])))
+X_tidy <-dcast(X_melt, subject_id+activ_name~variable,mean)
+
+##
+## Renaming variables names with a prefix "Avg_"
+## to indicate the values shown are the average  
+##
+newNames <- as.factor(c(names(X_tidy[,1:2]), (paste("Avg", names(X_tidy[,3:68]), sep="_"))))
+colnames(X_tidy) <- newNames
+
+
+##
+## Returning the final tidy data set
+##
+return(X_tidy)
+
+}
